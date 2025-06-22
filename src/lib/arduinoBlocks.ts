0cd4c459a1e420e41924b0fa148988382cec7eac
donrukanga1 <donaldrukanga@gmail.com>
2025-06-22 12:07:33 +0300

    Update arduinoBlocks.ts grok1

--- src/lib/arduinoBlocks.ts
+++ src/lib/arduinoBlocks.ts
@@ -1,6 +1,7 @@
 import * as Blockly from "blockly";
 
 export const defineArduinoBlocks = () => {
+  // Setup/Loop Block
   Blockly.Blocks["controls_setup"] = {
     init: function () {
       this.appendDummyInput().appendField("Arduino Setup & Loop");
@@ -11,11 +12,12 @@ export const defineArduinoBlocks = () => {
     },
   };
 
+  // LED Block
   Blockly.Blocks["component_led"] = {
     init: function () {
       this.appendDummyInput()
         .appendField("Set LED on pin")
-        .appendField(new Blockly.FieldNumber(13, 0, 21), "PIN")
+        .appendField(new Blockly.FieldNumber(13, 0, 21, 1), "PIN")
         .appendField("to")
         .appendField(new Blockly.FieldDropdown([["ON", "HIGH"], ["OFF", "LOW"]]), "STATE");
       this.setPreviousStatement(true, null);
@@ -24,8 +26,45 @@ export const defineArduinoBlocks = () => {
       this.setTooltip("Set the state of an LED.");
     },
   };
+
+  // IMU (LSM6DS3) Block
+  Blockly.Blocks["component_imu"] = {
+    init: function () {
+      this.appendDummyInput()
+        .appendField("Read IMU")
+        .appendField(
+          new Blockly.FieldDropdown([
+            ["Accelerometer X", "ACC_X"],
+            ["Accelerometer Y", "ACC_Y"],
+            ["Accelerometer Z", "ACC_Z"],
+            ["Gyroscope X", "GYRO_X"],
+            ["Gyroscope Y", "GYRO_Y"],
+            ["Gyroscope Z", "GYRO_Z"],
+          ]),
+          "SENSOR_TYPE"
+        );
+      this.setOutput(true, "Number");
+      this.setColour(200); // Sensor block color
+      this.setTooltip("Read accelerometer or gyroscope data from the IMU sensor.");
+    },
+  };
+
+  // Microphone (MP34DT05) Block
+  Blockly.Blocks["component_microphone"] = {
+    init: function () {
+      this.appendDummyInput().appendField("Read Microphone Level");
+      this.setOutput(true, "Number");
+      this.setColour(200); // Sensor block color
+      this.setTooltip("Read the audio level from the onboard microphone.");
+    },
+  };
 };
 
 export const areBlocksRegistered = () => {
-  return !!Blockly.Blocks["controls_setup"] && !!Blockly.Blocks["component_led"];
+  return (
+    !!Blockly.Blocks["controls_setup"] &&
+    !!Blockly.Blocks["component_led"] &&
+    !!Blockly.Blocks["component_imu"] &&
+    !!Blockly.Blocks["component_microphone"]
+  );
 };
